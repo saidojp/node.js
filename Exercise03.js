d458e934c806b0df12439856426bab22af2c2690
saidojp <saidlowxp@gmail.com>
2025-07-30 19:15:54 +0500

    Average Joe

---
+++ Exercise03.js
@@ -0,0 +1,7 @@
+const weekDaysCups = 10;
+const weekendCups = 4;
+function avarageCupsinDay() {
+  const result = (weekDaysCups + weekendCups) / 7;
+  console.log("Coffe consuption per day: " + result);
+}
+avarageCupsinDay();
