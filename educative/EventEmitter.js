3a5506ff6bcdd666599d786f6df189a9030d831e
saidojp <saidlowxp@gmail.com>
2025-08-16 15:26:14 +0500

    event emitter

---
+++ educative/EventEmitter.js
@@ -0,0 +1,21 @@
+const EventEmitter = require("events");
+
+const myEmitter = new EventEmitter();
+
+const placeOrder = function (food) {
+  myEmitter.emit("order", food);
+  myEmitter.emit("doorbell");
+  myEmitter.emit("payment", food);
+};
+
+myEmitter.on("order", (food) => {
+  console.log("Order placed for", food);
+});
+
+myEmitter.on("doorbell", () => {
+  console.log("RING RING!");
+});
+
+myEmitter.on("payment", (food) => {
+  console.log("Enjoy your", food);
+});
