938277c403640c3651421e0a6c4b9ed7b8a01b50
saidojp <saidlowxp@gmail.com>
2025-08-18 17:29:10 +0500

    touching graphql

---
+++ 06-GraphQL/query-test/server.js
@@ -0,0 +1,59 @@
+const apollo = require("apollo-server");
+const { ApolloServer, gql } = apollo;
+
+// this is fake representation of database pizza topping record
+// on the production environment you probably want real data from database
+const pizzaToppings = [{ id: 1, topping: "Cheesy" }];
+
+// this is fake representation of database record
+// on the production environment you probably want real data from database
+const pizzas = [
+  { id: 1, pizza: "Neapolitan Pizza", toppings: pizzaToppings },
+  { id: 2, pizza: "Chicago Pizza", toppings: pizzaToppings },
+  { id: 3, pizza: "New York-Style Pizza", toppings: pizzaToppings },
+  { id: 4, pizza: "Sicilian Pizza", toppings: pizzaToppings },
+  { id: 5, pizza: "Greek Pizza", toppings: pizzaToppings },
+  { id: 6, pizza: "California Pizza", toppings: pizzaToppings },
+  { id: 7, pizza: "Detroit Pizza", toppings: pizzaToppings },
+  { id: 8, pizza: "St. Louis Pizza", toppings: pizzaToppings },
+];
+
+const server = new ApolloServer({
+  playground: true,
+  typeDefs: gql`
+    # Schema Type
+    type Pizza {
+      id: Int!
+      pizza: String!
+      toppings: [Topping!]!
+    }
+
+    type Topping {
+      id: Int!
+      topping: String!
+    }
+
+    type Query {
+      pizzas(pizza: String): [Pizza]
+      pizza(id: Int): Pizza!
+    }
+  `,
+  resolvers: {
+    Query: {
+      pizzas: (parent, args, context) => {
+        const { pizza } = args;
+        const name = pizza;
+        if (name) {
+          // this is fake implementation of simple string matching
+          // on production environment you would want to query from real database!
+          return [pizzas.find(({ pizza }) => pizza === name)];
+        }
+        return pizzas;
+      },
+    },
+  },
+});
+
+server.listen({ port: 3000 }).then(({ url }) => {
+  console.log(`Server running at ${url}`);
+});
